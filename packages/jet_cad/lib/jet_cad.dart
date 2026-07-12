69ddc7df56779eeb4419cae0be1e71fb27594fb2
Ahmet Urel <a-urel@hotmail.com>
2026-07-12 04:19:57 +0300

    chore: scaffold pub workspace and jet_cad ffi plugin package

---
+++ packages/jet_cad/lib/jet_cad.dart
@@ -0,0 +1,2 @@
+/// A headless-first CAD engine and viewport for Flutter, backed by OCCT.
+library;
---
+++ packages/jet_cad/macos/Classes/jet_cad.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../jet_cad.podspec for more information.
+#include "../../src/jet_cad.c"
